ecdf5b6be2044cd08e67ac5da4e94b06872d3208
carloscardoso-prog <carloseduardocardsilva@gmail.com>
2024-01-12 13:31:51 -0300

    novos exercicios documentaçao

--- pratica/3/app.js
+++ pratica/3/app.js
@@ -2,9 +2,14 @@ new Vue({
     el: '#desafio',
     watch: {
         valor() {
-            setTimeout(()=>{
+            setTimeout(() => {
                 this.valor = 0;
             }, 5000);
+        }
+    },
+    computed: {
+        resultado() { 
+            return this.valor != 37? "valor diferente" : "valor igual";
         }
     },
     data: {
